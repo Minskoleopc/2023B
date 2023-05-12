7024beb5626cc4b481a6a7d3c6705d1dd2fcaa2a
Minskoleopc <minskoleopc@gmail.com>
2023-05-12 21:30:04 +0530

    added

---
+++ script7.js
@@ -0,0 +1,15 @@
+
+
+let numT = 5
+
+if(numT > 0 && numT <= 5){
+    console.log('10 % discount')
+}
+
+if(numT > 5 && numT <= 10){
+    console.log('10 % discount')
+}
+
+if(numT > 10){
+    console.log('10 % discount')
+}
