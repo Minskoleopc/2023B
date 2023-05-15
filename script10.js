d5edc03363361801a8294fef65a686b1d8cc284c
Minskoleopc <minskoleopc@gmail.com>
2023-05-15 21:31:01 +0530

    added

---
+++ script10.js
@@ -0,0 +1,40 @@
+// for loop 
+for(let i = 0 ; i <=3 ; i++){ //1 //2 // 3 // 4
+    console.log(i) // 0 // 1 // 2 // 3
+}
+
+// print hello three tyms
+
+for(let i = 1 ; i <=3 ; i++){ // 2 // 3 // 4
+    console.log('hello')
+    // hello
+    // hello
+    // hello
+}
+
+// print 1 to 5
+
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
+    console.log(i) // 1 // 2 // 3 // 4 // 5
+} 
+
+// print 5 to 1
+for(let i = 5 ; i >= 1 ; i--){ // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 // 4 // 3 // 2 // 1
+}
+
+// table of two using for loop
+for(let i = 2 ; i <= 20 ; i = i + 2){ // 4 // 6 // 8 // 10 ----- 22
+    console.log(i) //2 // 4 // 6 // 8 // 10 --------- 20
+}
+
+// table of 3 in reverse
+for(i = 30 ; i >= 3 ; i = i - 3){ // 27 ----------------0
+    console.log(i) // 30-------------------------3
+}
+
+// continue
+// break 
+
+
+
